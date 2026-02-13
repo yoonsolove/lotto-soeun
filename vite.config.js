dce4bc48b5a33e705f57705bdcf957cdc322f19d
yoonsolove <jyj68@naver.com>
2026-02-14 01:19:05 +0900

    vite PWA 설정 추가

--- vite.config.js
+++ vite.config.js
@@ -1,26 +1,28 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import { VitePWA } from 'vite-plugin-pwa';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
   plugins: [
     react(),
     VitePWA({
       registerType: 'autoUpdate',
-      includeAssets: ['favicon.svg', 'favicon.ico', 'robots.txt', 'apple-touch-icon.png'],
+      includeAssets: ['favicon.svg', 'icons/icon-192x192.png', 'icons/icon-512x512.png'],
       manifest: {
         name: '소은 로또 번호 생성기',
-        short_name: '소은 로또',
-        description: '은이가 만들어주는 운세 로또 앱',
+        short_name: '소은로또',
+        start_url: '.',
+        display: 'standalone',
+        background_color: '#ffffff',
         theme_color: '#ffffff',
         icons: [
           {
-            src: 'icons/icon-192x192.png',
+            src: '/icons/icon-192x192.png',
             sizes: '192x192',
             type: 'image/png'
           },
           {
-            src: 'icons/icon-512x512.png',
+            src: '/icons/icon-512x512.png',
             sizes: '512x512',
             type: 'image/png'
           }
@@ -28,4 +30,4 @@ export default defineConfig({
       }
     })
   ]
-});
+})
